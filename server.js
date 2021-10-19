9b8d5414b1d0a86b48320d9bd6fbcbba9e77c624
Akshay-VK <akshaykaruvathil@gmail.com>
2021-10-19 21:43:56 +0530

    Added : .gitignore

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+httpServer.listen(process.env.port||3000);
+console.log(`Server started at port ${process.env.port||3000}`);
