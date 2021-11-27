ccbc3cb42aebfdb923d2392c5a545e00dc3882b3
Akshay-VK <akshaykaruvathil@gmail.com>
2021-11-27 11:41:25 +0530

    added and set up p5.js

--- public/index.js
+++ public/index.js
@@ -1,8 +1,13 @@
 console.log("Asteroids!!");
 const socket = io();
-socket.emit("ping","pong!");
-socket.emit("init",{});
-socket.once("init-ripple",(arg)=>{
-    console.log(arg);
-});
-
+function setup(){
+    socket.emit("ping","pong!");
+    socket.emit("init",{});
+    socket.once("init-ripple",(arg)=>{
+        console.log(arg);
+    });
+    createCanvas(400,400);
+}
+function draw(){
+    background(0);
+}
