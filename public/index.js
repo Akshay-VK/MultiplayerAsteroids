424d0229d1d3dafbdb37c9fed2a70c240a89a6c0
Akshay-VK <akshaykaruvathil@gmail.com>
2021-12-14 21:05:01 +0530

    merged branch: showingothersprites

--- public/index.js
+++ public/index.js
@@ -2,12 +2,16 @@ console.log("Asteroids!!");
 const socket = io();
 var player;
 var drawable=false;
+var gameW=0;
+var gameH=0;
 function setup(){
     socket.emit("ping","pong!");
     socket.emit("init",{});
     socket.once("init-ripple",(arg)=>{
         console.log(arg);
-        player = new Player(socket, createVector(arg.x,arg.y),createVector(arg.dirX,arg.dirY));
+        gameW=arg.width;
+        gameH=arg.height;
+        player = new Player(socket, createVector(arg.x,arg.y),createVector(arg.dx,arg.dy),gameW,gameH);
         console.log(player);
         drawable=true;
     });
@@ -19,5 +23,9 @@ function draw(){
     if(drawable){
         player.update();
         player.show();
+        player.ping();
+        player.drawOthers();
+        console.log(1);
     }
 }
+socket.on("update-ripple",(arg)=>{if(drawable){player.updateState(arg);}});
--- public/player.js
+++ public/player.js
@@ -1,9 +1,12 @@
 class Player {
-  constructor(socket, pos, dir) {
+  constructor(socket, pos, dir, w, h) {
     this.skt = socket;
     this.pos = pos;
     this.dir = dir;
     this.angle = 0;
+    this.w = w;
+    this.h = h;
+    this.local_state = {};
   }
   update() {
     angleMode(DEGREES);
@@ -16,6 +19,9 @@ class Player {
     this.dir.x = cos(this.angle);
     this.dir.y = sin(this.angle);
     this.pos.add(this.dir);
+
+    this.pos.x = Math.max(Math.min(this.pos.x, this.w / 2), -this.w / 2);
+    this.pos.y = Math.max(Math.min(this.pos.y, this.h / 2), -this.h / 2);
   }
   show() {
     push();
@@ -45,8 +51,67 @@ class Player {
     this.skt.emit("beat", {
       x: this.pos.x,
       y: this.pos.y,
-      dirX: this.dir.x,
-      dirY: this.dir.y
+      dx: this.dir.x,
+      dy: this.dir.y,
+      angle: this.angle
     });
   }
+  drawOthers() {
+    var arg = this.local_state;
+    var keys = Object.keys(arg);
+    console.log(arg);
+    //console.log(keys);
+    for (var i = 0; i < keys.length; i++) {
+      if (keys[i] != this.skt.id) {
+        var x = arg[keys[i]].x - this.pos.x + width / 2;
+        var y = arg[keys[i]].y - this.pos.y + height / 2;
+
+        // push();
+        // translate(arg[keys[i]].x-this.pos.x,arg[keys[i]].y-this.pos.y);
+        // translate(width/2,height/2);
+        // rotate(arg[keys[i]].angle);
+        // noFill();
+        // stroke(255);
+        // strokeWeight(2);
+        // beginShape();
+        // vertex(10, 0);
+        // vertex(-5, -5);
+        // vertex(-5, 5);
+        // pop();
+        fill(255);
+        rect(x, y, 5, 5);
+        //console.log(1);
+      }
+    }
+  }
+  updateState(arg) {
+    this.local_state = arg;
+  }
+  // drawOthers(arg){
+  //   var keys = Object.keys(arg);
+  //   console.log(arg);
+  //   //console.log(keys);
+  //   for(var i = 0; i < keys.length;i++){
+  //     if(keys[i] != this.skt.id){
+  //       var x = (arg[keys[i]].x-this.pos.x)+(width/2);
+  //       var y = (arg[keys[i]].y-this.pos.y)+(height/2);
+
+  //       // push();
+  //       // translate(arg[keys[i]].x-this.pos.x,arg[keys[i]].y-this.pos.y);
+  //       // translate(width/2,height/2);
+  //       // rotate(arg[keys[i]].angle);
+  //       // noFill();
+  //       // stroke(255);
+  //       // strokeWeight(2);
+  //       // beginShape();
+  //       // vertex(10, 0);
+  //       // vertex(-5, -5);
+  //       // vertex(-5, 5);
+  //       // pop();
+  //       fill(255);
+  //       rect(x,y,5,5);
+  //       //console.log(1);
+  //     }
+  //  }
+  //}
 }
